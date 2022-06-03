c295d014a936a689593f5c022ea46b18824fedb8
eduardo <edup2306@gmail.com>
2022-06-03 20:09:51 +1000

    Fixing typos

--- src/main/java/com/au/code/ImdbApiExplorerApplication.java
+++ src/main/java/com/au/code/ImdbApiExplorerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImbdApiExprolerApplication {
+public class ImdbApiExplorerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImbdApiExprolerApplication.class, args);
+		SpringApplication.run(ImdbApiExplorerApplication.class, args);
 	}
 
 }
